8b64ab114d13eba332b2b36d715b8b4677ab316b
Markel <mkaluh@softserveinc.com>
2016-08-25 00:47:12 +0300

    Added function for reloading router after main page load

--- apps/web_ui/scripts/app.js
+++ apps/web_ui/scripts/app.js
@@ -5,32 +5,25 @@ var app = angular.module("bulavkaApp", ['ngRoute']);
 
 app.config(function($routeProvider, $locationProvider){
 
-        $locationProvider.html5Mode({
-            enabled: true,
-            requireBase: false
-        });
+        $locationProvider.html5Mode(true);
+        $locationProvider.hashPrefix('!');
 
         $routeProvider
             .when('/', {
                 templateUrl: '/template/home.html',
                 controller: 'MainController',
-                useAsDefault: true
-
             })
             .when('/about_us', {
                 templateUrl: '/template/about_us.html',
                 controller: 'AboutController'
-
             })
             .when('/articles', {
                 templateUrl: '/template/articles.html',
                 controller: 'ArticlesController'
-
             })
             .when('/contacts', {
                 templateUrl: '/template/contacts.html',
                 controller: 'ContactsController'
-
             })
             .otherwise({
                 redirectTo: '/'
@@ -39,50 +32,8 @@ app.config(function($routeProvider, $locationProvider){
     }
 );
 
-app.controller('MainController', function($scope, $http) {
-    $scope.items = [];
-
-
-    $http.get('http://bulavka:8080/api/v1/products')
-        .success(function(responce) {
-            $scope.items = responce;
-        }
-    );
-
-});
-
-app.controller('ContactsController', function($scope, $http) {
-    $scope.items = [];
-
-
-    $http.get('./scripts/json/item-example.json')
-        .success(function(responce) {
-            $scope.items = responce;
-        }
-    );
-
-});
-
-app.controller('ArticlesController', function($scope, $http) {
-    $scope.items = [];
-
-
-    $http.get('./scripts/json/item-example.json')
-        .success(function(responce) {
-            $scope.items = responce;
-        }
-    );
-
-});
-
-app.controller('AboutController', function($scope, $http) {
-    $scope.items = [];
-
-
-    $http.get('./scripts/json/item-example.json')
-        .success(function(responce) {
-            $scope.items = responce;
-        }
-    );
-
-});
+app.run(['$rootScope', '$route', function ($rootScope, $route) {
+    $rootScope.mainPageOnLoad = function () {
+        $route.reload()
+    };
+}]);
