4adc230f8929c0c74bef2ca18172b69d762f8f96
markel.kalugin <markel.kalugin@gmail.com>
2016-12-11 23:46:10 +0200

    1. Changed call format for getting initial data 2. Implemented main functionality for filters on categories page

--- apps/web_ui/scripts/controllers/categories.js
+++ apps/web_ui/scripts/controllers/categories.js
@@ -7,6 +7,7 @@ app.controller('CategoriesController', function ($rootScope, $scope, $http, $rou
     $scope.categoriesFilterCondition = [];
     $scope.categories = [];
     $scope.Filter = new Object();
+    $scope.categoriesFilterModel = {checkbox: []};
 
     var currentCategoryName = '';
 
@@ -16,9 +17,7 @@ app.controller('CategoriesController', function ($rootScope, $scope, $http, $rou
         currentCategoryName = $location.path().split('/')[2];
     }
 
-    $scope.filtersIds = InitialData.categoriesFilterCondition[currentCategoryName] || [];
-
-    var getContent = function () {
+    var getContent = function (filtersIds, itemName) {
         $http({
             method: 'POST',
             url: $rootScope.apiUrl + $rootScope.baseUrl + 'categories',
@@ -27,30 +26,63 @@ app.controller('CategoriesController', function ($rootScope, $scope, $http, $rou
                 'Accept': 'application/json'
             },
             data: {
-                categories_filter_conditions: $scope.filtersIds
+                categories_filter_conditions: filtersIds
             }
         }).success(function (response) {
-                $scope.categoriesPage.products = response.products;
-                $scope.categoriesPage.filters = response.filters;
-                return $scope.categoriesPage;
+                $scope.categoriesPage[itemName] = response[itemName];
             }
         );
     };
 
-    getContent();
+    InitialData.async().then(function (responce) {
+        $scope.filtersIdsQueryString = $location.search()['f'];
+        $scope.filtersIds = responce['categoriesFilterCondition'][currentCategoryName];
+        var filterIdsQueryStringValidation = function () {
+            if ($scope.filtersIdsQueryString) {
+                if (typeof $scope.filtersIdsQueryString == 'string') {
+                    $scope.filtersIdsQueryString = [parseInt($scope.filtersIdsQueryString)];
+                } else if (typeof $scope.filtersIdsQueryString == 'undefined') {
+                    $scope.filtersIdsQueryString = [];
+                }
+            } else {
+                $scope.filtersIdsQueryString = [$scope.filtersIds[0]]
+            }
+        };
+
+        filterIdsQueryStringValidation();
+
+        $scope.filtersIdsQueryString.forEach(function (element) {
+            if (!$scope.filtersIds.includes(parseInt(element))) {
+                $scope.filtersIds.push(parseInt(element));
+            }
+        });
+
+        getContent($scope.filtersIds, 'products');
+        getContent([$scope.filtersIds[0]], 'filters');
+
+        filterIdsQueryStringValidation();
+
+        $scope.filtersIdsQueryString.forEach(function (element) {
+            if (!$scope.categoriesFilterModel.checkbox.includes(parseInt(element))) {
+                $scope.categoriesFilterModel.checkbox[element] = true
+            }
+        });
+    });
 
     $scope.filterCheck = function () {
         var checkboxes = document.getElementsByClassName('filter-checkbox');
+        var mainCondition = [$scope.filtersIds[0]];
         for (var i = 0; i < checkboxes.length; ++i) {
+            $scope.filtersIds = mainCondition;
             if (checkboxes[i].checked) {
-                $scope.filtersIds.push(parseInt(checkboxes[i].id))
+                $scope.filtersIds.push(parseInt(checkboxes[i].id));
             }
         }
-        console.log($scope.filtersIds);
-        $timeout(function () {
-            getContent();
-            $scope.$apply();
-        }, 100);
-    }
+        $location.search('f', $scope.filtersIds);
+        getContent($scope.filtersIds, 'products');
+    };
 
+    $scope.isFilterChecked = function (filterId) {
+        return !!$scope.filtersIds.includes(filterId);
+    }
 });
