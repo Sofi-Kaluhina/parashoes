6f8615359d860f50e185bf841bec089a791ee007
sofi.kalugina <sofi.kalugina@gmail.com>
2016-12-24 19:23:34 +0200

    Changes in blocks positions Map scrollwheel is disabled

--- apps/web_ui/scripts/controllers/contacts.js
+++ apps/web_ui/scripts/controllers/contacts.js
@@ -2,14 +2,38 @@
  * Created by markel on 16.08.16.
  */
 
-app.controller('ContactsController', function($scope, $http) {
-    $scope.items = [];
+app.controller('ContactsController', function($scope) {
 
+    $scope.initialize = (function () {
+        $scope.myLatlng = new google.maps.LatLng(47.100035, 37.553630);
+        $scope.myOptions = {
+            zoom: 16,
+            center: $scope.myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scrollwheel: false
+        };
+        $scope.map = new google.maps.Map(document.getElementById("map_canvas"), $scope.myOptions);
 
-    // $http.get('./scripts/json/item-example.json')
-    //     .success(function(responce) {
-    //         $scope.items = responce;
-    //     }
-    // );
+        $scope.marker = new google.maps.Marker({
+            position: $scope.myLatlng,
+            map: $scope.map,
+            title:"Мы тут!"
+        });
 
+        $scope.contentString = '<div id="content">' +
+            '<p style="font-weight: bold">' + '"BUlavka"' + '</p>' +
+            'ул. Артема, 59' +
+            '<p>' + 'Пн-Вс с 9:00 до 19:00' + '</p>' +
+            '<a target="_blank" href="https://maps.google.com/maps?ll=47.100236,37.553397&z=18&t=m&hl=ru-RU&gl=US&mapclient=apiv3&cid=15462233706182546960">' +
+            '<p>' + ' Показать на Google Картах'+ '</p>' +
+            '</a>' +
+            '</div>';
+
+        $scope.infowindow = new google.maps.InfoWindow({
+            content: $scope.contentString
+        });
+        google.maps.event.addListener($scope.marker, 'click', function() {
+            $scope.infowindow.open($scope.map,$scope.marker);
+        });
+    })();
 });
